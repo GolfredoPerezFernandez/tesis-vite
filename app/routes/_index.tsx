d250ea38c51c0f01b5e5123e2f22547f99e7ae60
Golfredo Perez Fernandez <golfredo.pf@gmail.com>
2023-11-19 15:28:09 -0400

    asd

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,6 +11,8 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Remix</h1>
      
     </div>
   );
